38203cc0f3766d7fef9aa0e60c5e5e170966603e
ParthoKR <partho.rajvor2016@gmail.com>
2020-09-05 09:51:42 +0600

    Updated models.py with question and choice field

--- Week 1/Assignment/models.py
+++ Week 1/Assignment/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    # publish date is optional you can skip that field
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
