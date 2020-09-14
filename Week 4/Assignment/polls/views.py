47aea3ec783e4eecc9a618a25a45476de6984e89
ParthoKR <partho.rajvor2016@gmail.com>
2020-09-14 21:31:27 +0600

    Updated week 4 readme

--- Week 4/Assignment/polls/views.py
+++ Week 4/Assignment/polls/views.py
@@ -2,20 +2,8 @@ from django.shortcuts import HttpResponse, render, get_object_or_404, HttpRespon
 from polls.models import Question,  Choice
 from django.views import generic
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-#     response = "You're looking at the results of question %s."
-    # return HttpResponse(response % question_id)
-
 def owner(request):
-    return HttpResponse("Hello, world. cb59b747 is the polls owner.")
+    return HttpResponse("Hello, world. <YOUR_STRING_HERE> is the polls owner.") # replace <YOUR_STRING_HERE> with the string you get when submitting assignment
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
